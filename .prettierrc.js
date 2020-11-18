b66811812fd86e9f2e3c45955a474d13f9bf0861
SHRIHARI MURALI <shriharim6@gmail.com>
2020-11-18 20:59:43 +0530

    Initial Commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
